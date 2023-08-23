555b418af12a548fe59f1cd9c32257cabddaae34
Renan Dias <112900871+WHrez1ns@users.noreply.github.com>
2023-08-22 21:03:13 -0300

    ScanMatrix-Pro - 2.3
    - Definindo valores padrões para os scans
    - Alterando opções para opções do nmap mais relevantes e usadas
    - Corrigindo a lógica de detecção do fiware (agora funcionando)
    - Arrumando outros detalhes bobos de CMD Style

--- ScanMatrixPro.py
+++ ScanMatrixPro.py
@@ -23,9 +23,13 @@ def line():
 def scan(argument, scan_type_name):
 	try:
 		host_address = input(Colors.BLUE + "Provide a valid Host address\n" + Colors.ENDC + ': ')
+		if host_address == '':
+			host_address = "0.0.0.0"
 		print(Colors.WARNING + f"Host: {host_address}" + Colors.ENDC)
 		line()
 		range = input(Colors.BLUE + "Provide a valid Range or Port/Ports | Example: 1-1024 \n" + Colors.ENDC + ': ')
+		if range == '':
+			range = "1-1024"
 		print(Colors.WARNING + f"Range: {range}" + Colors.ENDC)
 		line()
 		print(Colors.GREEN + 'Starting Scan' + Colors.ENDC)
@@ -61,28 +65,39 @@ def scan(argument, scan_type_name):
 
 def fingerprint_scan(argument):
 	try:
-		host_address = "0.0.0.0"
-		range = "1026, 1883, 4041, 8666, 9001, 27017"
+		host_address = input(Colors.BLUE + "Provide a valid Host address | Recommended: 0.0.0.0\n" + Colors.ENDC + ': ')
+		if host_address == '':
+			host_address = "0.0.0.0"
+		print(Colors.WARNING + f"Host: {host_address}" + Colors.ENDC)
+		line()
+		fiware_range = "1026, 1883, 4041, 8666, 9001, 27017"
 		fiware_ports = ["1026", "1883", "4041", "8666", "9001", "27017"]
 		fiware_status = []
-		nm.scan(host_address, range, arguments=argument)
+		nm.scan(host_address, fiware_range, arguments=argument)
 		time.sleep(3)
 		for host in nm.all_hosts():
 			if nm[host].state() == "down":
 				print(Colors.FAIL + "Non-existent or inactive host" + Colors.ENDC)
 				line()
 			else:
+				print(Colors.HEADER + "* Fingerprint Analysis *" + Colors.ENDC)
 				for proto in nm[host].all_protocols():
+					line()
+					print(f'Protocol : {proto}')
 					lport = nm[host][proto].keys()
 					for port in lport:
 						state = nm[host][proto][port]['state']
-						# service_name = nm[host][proto][port]['name']
-						for fiware_port in fiware_ports:
-							if int(fiware_port) == port and state == "open":
-								fiware_status.append(1)
+						service_name = nm[host][proto][port]['name']
+						print("[+] " + Colors.WARNING + f'Port : {port}\t\t' + 
+							  	    Colors.GREEN + f'State : {state}\t\t' + 
+							  		Colors.BLUE + f'Service : {service_name}' + Colors.ENDC)
+						if not port in fiware_ports and state == "open":
+							fiware_status.append(1)
+						else:
+							fiware_status.append(0)
 				if not 0 in fiware_status:
-					print("[+] " + Colors.GREEN + '"FIWARE" Detected on server' + Colors.ENDC)
-				# print(fiware_status)				
+					line()
+					print("[!] " + Colors.WARNING + '"FIWARE" Detected on server' + Colors.ENDC)				
 		line()
 	except xml.etree.ElementTree.ParseError:
 			print(Colors.FAIL + "Permission error | Try running with: sudo ./scanmatrixpro.py" + Colors.ENDC)
@@ -100,41 +115,41 @@ print(Colors.HEADER + " \___ \  / __|/ _` || '_ \ | |\/| | / _` || __|| '__|| |\
 print(Colors.HEADER + "  ___) || (__| (_| || | | || |  | || (_| || |_ | |   | | >  <|_____||  __/ | |  | (_) | " + Colors.ENDC)
 print(Colors.HEADER + " |____/  \___|\__,_||_| |_||_|  |_| \__,_| \__||_|   |_|/_/\_\      |_|    |_|   \___/  " + Colors.ENDC)
 print("")
-print(Colors.FAIL + "                                                      ScanMatrix-Pro v2.2 - by Renan D. " + Colors.ENDC)
+print(Colors.FAIL + "                                                      ScanMatrix-Pro v2.3 - by Renan D. " + Colors.ENDC)
                                                                                        
 
 while True:
 	try:
-		type_scan = int(input(Colors.BLUE + "Select a Scan type:" + Colors.ENDC + "\n[1] SYN Scan\n[2] UDP Scan\n[3] Connect Scan\n[4] Fin Scan\n[5] Xmas Scan\n[6] Custom Scan\n[7] Fingerprint Analysis\n[8] Exit\n: "))
+		type_scan = int(input(Colors.BLUE + "Select a option:" + Colors.ENDC + "\n[1] TCP SYN scan\n[2] TCP connect scan\n[3] UDP scan\n[4] Aggressive scan\n[5] OS detection\n[6] Custom scan\n[7] Fingerprint Analysis\n[8] Exit\n: "))
 		line()
-		# SYN Scan
+		# TCP SYN scan
 		if type_scan == 1:
 			try:
-				scan('-sS', 'SYN Scan')
+				scan('-sS', 'TCP SYN scan')
 			except:
 				print(Colors.FAIL + "Unexpected error" + Colors.ENDC)
-		# UDP Scan
+		# TCP connect scan
 		elif type_scan == 2:
 			try:
-				scan('-sU', 'UDP Scan')
+				scan('-sT', 'TCP connect scan')
 			except:
 				print(Colors.FAIL + "Unexpected error" + Colors.ENDC)
-		# Connect Scan
+		# UDP scan
 		elif type_scan == 3:
 			try:
-				scan('-sT', 'Connect Scan')
+				scan('-sU', 'UDP scan')
 			except:
 				print(Colors.FAIL + "Unexpected error" + Colors.ENDC)
-		# Fin Scan
+		# Aggressive scan
 		elif type_scan == 4:
 			try:
-				scan('-sF', 'Fin Scan')
+				scan('-A', 'Aggressive scan')
 			except:
 				print(Colors.FAIL + "Unexpected error" + Colors.ENDC)
-		# Xmas Scan
+		# OS detection
 		elif type_scan == 5:
 			try:
-				scan('-sX', 'Xmas Scan')
+				scan('-O', 'OS detection')
 			except:
 				print(Colors.FAIL + "Unexpected error" + Colors.ENDC)
 		# Custom Scan
@@ -151,7 +166,7 @@ while True:
 			fingerprint_scan("")
 		# Exit
 		elif type_scan == 8:
-			print(Colors.WARNING + 'Until later :D')
+			print(Colors.HEADER + 'Until later :D')
 			exit()
 		# Error
 		else:
